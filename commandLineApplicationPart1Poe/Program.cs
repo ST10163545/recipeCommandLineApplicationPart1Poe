8abe9a79e4d6d59b06c353f4a3ddfd99c13447e9
Muhammed <Muhammed@DESKTOP-MBC6DA8>
2023-04-26 17:16:15 +0200

    Add project files.

---
+++ commandLineApplicationPart1Poe/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandLineApplicationPart1Poe
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            int p = 1;
+            while (p != 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("choose one of the following options below \n" +
+                    "1. Enter the details for a single recipe \n" +
+                    "2. Display the full recipe \n" +
+                    "3. Scale the recipe \n" +
+                    "4. Reset quantities to its orignal values \n" +
+                    "5. Clear all data to enter a new recipe \n" +
+                    "6. Exit application ");
+
+
+                int option = int.Parse(Console.ReadLine());
+                if (option == 1)
+                {
+                    string recipes = recipe();
+                    report.recipes = recipes;
+                    int num =  numIngredients() ;
+                    string[] names = new string[num];
+                    //string[] recipe = new string[num];
+                    double [] quant = new double[num];
+                    string [] measure = new string[num];
+                    string[] stepdesc= new string[num];
+                    
+                    for (int i = 0; i < num; i++)
+                    {
+                        //recipes[i]= recipe();
+                        names [i] = name(i);
+                        quant[i] = quantity(i);
+                        measure[i] = measur();
+                        
+                    }
+                    int ste = step();
+                    for (int j = 0; j < ste; j++)
+                    {
+                        stepdesc[j] = stepdes(j);
+                    }
+                    report.name = names;
+                    report.quants= quant;
+                    report.measures = measure;
+                    report.step = stepdesc;
+                }
+                else if (option == 2)
+                {
+
+
+                    report.display();
+                }
+                else if (option == 3)
+                {
+                    report.scale();
+                }
+                else if (option == 4)
+                {
+                     report.reset();
+                }
+                else if (option == 5)
+                {
+                    report.clear();
+                }
+                else if (option == 6)
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("wrong input");
+                }
+
+            }
+
+
+        }
+        public static string recipe()
+        {
+            Console.WriteLine("Please enter the name of Recipe ");
+            string rec = Console.ReadLine();    
+            return rec;
+        }
+        public static int numIngredients()
+        {
+            Console.WriteLine("Enter the number of ingredients needed for the recipe");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+        public static string name(int i)
+        {
+            Console.WriteLine("Please enter the name of Ingredient " + (i+1) + ": ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        public static double quantity(int i)
+        {
+            Console.WriteLine("Please enter the quantity for ingredient " + (i+1));
+            double quantity = double.Parse(Console.ReadLine());
+            return quantity;
+        }
+        public static string measur()
+        {
+            Console.WriteLine("Choose the unit of meaurment \n" +
+                "1. Teaspoon \n" +
+                "2. Tablespoon \n" +
+                "3. Cup");
+            //Console.WriteLine(Convert.ToInt32(Console.ReadLine()));
+            
+            int measur = Convert.ToInt32(Console.ReadLine());
+            
+            string measurement = "";
+            if (measur == 1)
+            {
+
+                measurement = "Teaspoon";
+            }
+            else if (measur == 2)
+            {
+
+                measurement = "Tablespoon";
+            }
+            else if (measur == 3)
+            {
+                measurement = "Cup";
+            }
+            return measurement;
+        }
+        
+        public static int step()
+        {
+            Console.WriteLine("Enter the number of steps required for the recipe: ");
+            int step = int.Parse(Console.ReadLine());
+            return step;
+        }
+        public static string stepdes(int i)
+        {
+            Console.WriteLine("Please enter step "+ (i+1)+": ");
+            String stepDes = Console.ReadLine();
+            return stepDes;
+        }
+
+    }
+}
---
+++ commandLineApplicationPart1Poe/report.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace commandLineApplicationPart1Poe
+{
+    internal class report
+    {
+        public static string recipes;
+        public static int[] nums;
+        public static string [] name;
+        public static double[] quants;
+        public static string[] measures;
+        public static string[] step;
+         public static double [] ab ;
+        // ab = quants;
+        
+        public static void display()
+        {
+            //string bb = "";
+            //string bc = "";
+            StringBuilder bc = new StringBuilder();
+            StringBuilder bb = new StringBuilder();
+            ab = quants;
+            for (int i = 0; i < name.Length; i++)
+            {
+
+                bc.Append(ab[i] +" " + measures[i] +" " + name[i]+ "\n");
+            }
+            for (int i = 0; i < step.Length; i++)
+            {
+                bb.Append("Step " + (i + 1) + ": " + step[i] +"\n");
+            }
+            
+            Console.WriteLine("*********************Recipe for "+ recipes+ " *********************** \n" +
+                "********************************************* \n" +
+                "Ingredients: \n" +
+                 bc+" \n" +
+                "Steps: \n" +
+                bb +"\n"+
+                "************************************************");
+
+             Console.ReadLine();
+        }
+
+        
+
+        public static double scale()
+        {
+            Console.WriteLine("recipe should be scaled to wich factor: \n" +
+                        "1. Half (0.5) \n" +
+                        "2. Double (2) \n" +
+                        "3. Triple (3)");
+            int opt = int.Parse(Console.ReadLine());
+            StringBuilder bc = new StringBuilder();
+            StringBuilder bb = new StringBuilder();
+            double  ab =0 ;
+            for (int i = 0; i < quants.Length; i++)
+            {
+
+                if (opt == 1)
+                {
+                    ab= quants[i] / 2;
+                }
+                else if (opt == 2)
+                {
+                    ab=quants[i] * 2;
+                }
+                else if (opt == 3)
+                {
+                    ab =quants[i] * 3;
+                }
+                
+            }
+            
+            for (int i = 0; i < name.Length; i++)
+            {
+                bc.Append(ab + " " + measures[i] + " " + name[i] +"\n");
+            }
+            for (int i = 0; i < step.Length; i++)
+            {
+                 bb.Append("Step "+ (i+1)+": "+step[i] + "\n");
+            }
+            Console.WriteLine("****************Recipe for "+ recipes+ " ************************ \n" +
+                "************************************************ \n" +
+                "Ingredients: \n" +
+                 bc + " \n" +
+                "Steps: \n" +
+                bb +"\n"+
+                "***************************************************");
+            return ab;
+            //Console.ReadLine();
+        }
+        public static void reset()
+        {
+            for (int i = 0; i < quants.Length; i++)
+            {
+                quants= ab;
+                
+            }
+            Console.WriteLine("********************************************* \n" +
+                "******************* Values reset******************* \n" +
+                "****************************************************");
+            
+        }
+
+        
+        public static void clear()
+        {
+
+            nums = new int[0];
+            name = new string[0];
+            quants = new double[0];
+            measures = new string[0];
+            step = new string[0];
+            ab = new double[0];
+            Console.WriteLine("******************************************** \n" +
+                "**************** All VALUES CLEARED *********************** \n" +
+                "*****************************************************");
+        }
+    }
+}
