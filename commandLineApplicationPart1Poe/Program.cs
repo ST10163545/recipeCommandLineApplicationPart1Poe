3f30c1cdb754ca232c8875fe409fbeeacd5c1934
Muhammed Nana <ST10163545@rcconnect.edu.za>
2023-04-26 18:40:32 +0200

    comment

--- commandLineApplicationPart1Poe/Program.cs
+++ commandLineApplicationPart1Poe/Program.cs
@@ -46,9 +46,9 @@ namespace commandLineApplicationPart1Poe
                         measure[i] = measur();
                         
                     }
-                    int ste = step();                           //
-                   
-                    for (int j = 0; j < ste; j++)
+                    int ste = step();                           //varable to store number of steps
+
+                    for (int j = 0; j < ste; j++)               //loop to get steps 
                     {
                         stepdesc[j] = stepdes(j);
                     }
@@ -57,25 +57,24 @@ namespace commandLineApplicationPart1Poe
                     report.measures = measure;
                     report.step = stepdesc;
                 }
-                else if (option == 2)
+                else if (option == 2)                       //if statement for second option
                 {
 
-
-                    report.display();
+                       report.display();
                 }
-                else if (option == 3)
+                else if (option == 3)                   //if statement for third option
                 {
                     report.scale();
                 }
-                else if (option == 4)
+                else if (option == 4)                   //if statement for fourth option
                 {
                      report.reset();
                 }
-                else if (option == 5)
+                else if (option == 5)                   //if statement for fifth option
                 {
                     report.clear();
                 }
-                else if (option == 6)
+                else if (option == 6)                   //if statement for sixth option
                 {
                     System.Environment.Exit(0);
                 }
@@ -88,37 +87,36 @@ namespace commandLineApplicationPart1Poe
 
 
         }
-        public static string recipe()
+        public static string recipe()                           //method to enter name of recipe
         {
             Console.WriteLine("Please enter the name of Recipe ");
             string rec = Console.ReadLine();    
             return rec;
         }
-        public static int numIngredients()
+        public static int numIngredients()          //method to enter number of ingredients
         {
             Console.WriteLine("Enter the number of ingredients needed for the recipe");
             int num = int.Parse(Console.ReadLine());
             return num;
         }
-        public static string name(int i)
+        public static string name(int i)            //method to enter the name of the ingredient
         {
             Console.WriteLine("Please enter the name of Ingredient " + (i+1) + ": ");
             string name = Console.ReadLine();
             return name;
         }
-        public static double quantity(int i)
+        public static double quantity(int i)        //method to enter quantity
         {
             Console.WriteLine("Please enter the quantity for ingredient " + (i+1));
             double quantity = double.Parse(Console.ReadLine());
             return quantity;
         }
-        public static string measur()
+        public static string measur()           //method to capture unit of measure
         {
             Console.WriteLine("Choose the unit of meaurment \n" +
                 "1. Teaspoon \n" +
                 "2. Tablespoon \n" +
                 "3. Cup");
-            //Console.WriteLine(Convert.ToInt32(Console.ReadLine()));
             
             int measur = Convert.ToInt32(Console.ReadLine());
             
@@ -140,13 +138,13 @@ namespace commandLineApplicationPart1Poe
             return measurement;
         }
         
-        public static int step()
+        public static int step()            //method to enter number of steps
         {
             Console.WriteLine("Enter the number of steps required for the recipe: ");
             int step = int.Parse(Console.ReadLine());
             return step;
         }
-        public static string stepdes(int i)
+        public static string stepdes(int i)         //method to enter steps
         {
             Console.WriteLine("Please enter step "+ (i+1)+": ");
             String stepDes = Console.ReadLine();
